23dbe453151e2aeb5cfaba965566e25c947a1caf
AmirChek7 <amirbekmamasaliev3@gmail.com>
2022-02-17 11:32:47 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let a = 'git init'; // registr repository
+let b = 'ls -a' // check that you really reg your repository
+let c = 'git add .' // . - means that you reg all files or you can reg writing file's name
+let d = 'git status' // check files you've add to list
+let e = 'git commit -m "first commit"' //you can, or can't
+let f = 'git remote add origin [link from github - ssh]' // connecting
+let g = 'git branch' // check which branch we are on
+let h = 'git push' // push
